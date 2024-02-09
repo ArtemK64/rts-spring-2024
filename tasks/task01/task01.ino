9996296190b89188e9fb65d366ebfcbbf5eeaac7
ArtemK64 <79468247+ArtemK64@users.noreply.github.com>
2024-02-09 16:09:39 +0400

    First attempt

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,7 +1,24 @@
-void setup() {
+const int pins[] = {3, 5, 6, 9, 10};
+const unsigned long intervals[] = {10000, 1000, 500, 100, 50};
+unsigned long previousMicros[] = {0, 0, 0, 0, 0};
 
+bool pinStates[] = {LOW, LOW, LOW, LOW, LOW}; 
+
+void setup() {
+  for (int i = 0; i < 5; i++) {
+    pinMode(pins[i], OUTPUT);
+  }
 }
 
 void loop() {
+  unsigned long currentMicros = micros();
 
+  for (int i = 0; i < 5; i++) {
+    if (currentMicros - previousMicros[i] >= intervals[i] / 2) {
+      previousMicros[i] = currentMicros;
+
+      pinStates[i] = !pinStates[i];
+      digitalWrite(pins[i], pinStates[i]);
+    }
+  }
 }
