4391a06cebb50fde38f82d5232fc05def35a3210
ArtemK64 <79468247+ArtemK64@users.noreply.github.com>
2024-02-09 16:12:22 +0400

    Second attempt

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -1,24 +1,53 @@
-const int pins[] = {3, 5, 6, 9, 10};
-const unsigned long intervals[] = {10000, 1000, 500, 100, 50};
-unsigned long previousMicros[] = {0, 0, 0, 0, 0};
+unsigned long previousMicros3 = 0;
+unsigned long previousMicros5 = 0;
+unsigned long previousMicros6 = 0;
+unsigned long previousMicros9 = 0;
+unsigned long previousMicros10 = 0;
 
-bool pinStates[] = {LOW, LOW, LOW, LOW, LOW}; 
+bool pinState3 = LOW;
+bool pinState5 = LOW;
+bool pinState6 = LOW;
+bool pinState9 = LOW;
+bool pinState10 = LOW;
 
 void setup() {
-  for (int i = 0; i < 5; i++) {
-    pinMode(pins[i], OUTPUT);
-  }
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
 }
 
 void loop() {
   unsigned long currentMicros = micros();
 
-  for (int i = 0; i < 5; i++) {
-    if (currentMicros - previousMicros[i] >= intervals[i] / 2) {
-      previousMicros[i] = currentMicros;
+  if (currentMicros - previousMicros3 >= 10000) {
+    previousMicros3 = currentMicros;
+    pinState3 = !pinState3;
+    digitalWrite(3, pinState3);
+  }
 
-      pinStates[i] = !pinStates[i];
-      digitalWrite(pins[i], pinStates[i]);
-    }
+  if (currentMicros - previousMicros5 >= 1000) {
+    previousMicros5 = currentMicros;
+    pinState5 = !pinState5;
+    digitalWrite(5, pinState5);
+  }
+
+  if (currentMicros - previousMicros6 >= 500) {
+    previousMicros6 = currentMicros;
+    pinState6 = !pinState6;
+    digitalWrite(6, pinState6);
+  }
+
+  if (currentMicros - previousMicros9 >= 100) {
+    previousMicros9 = currentMicros;
+    pinState9 = !pinState9;
+    digitalWrite(9, pinState9);
+  }
+
+  if (currentMicros - previousMicros10 >= 50) {
+    previousMicros10 = currentMicros;
+    pinState10 = !pinState10;
+    digitalWrite(10, pinState10);
   }
 }
