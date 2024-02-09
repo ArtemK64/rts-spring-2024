6e81ccf190188d291318b7df1e938f6cbe41bec4
ArtemK64 <79468247+ArtemK64@users.noreply.github.com>
2024-02-09 17:40:19 +0400

    Final attempt

--- tasks/task01/task01.ino
+++ tasks/task01/task01.ino
@@ -11,43 +11,45 @@ bool pinState9 = LOW;
 bool pinState10 = LOW;
 
 void setup() {
-  pinMode(3, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
+  DDRD |= (1 << DDD3) | (1 << DDD5) | (1 << DDD6);
+  DDRB |= (1 << DDB1) | (1 << DDB2);
 }
 
 void loop() {
   unsigned long currentMicros = micros();
 
-  if (currentMicros - previousMicros3 >= 10000) {
+  if (currentMicros - previousMicros3 >= 5000) {
     previousMicros3 = currentMicros;
     pinState3 = !pinState3;
-    digitalWrite(3, pinState3);
+    if (pinState3) PORTD |= (1 << PD3);
+    else PORTD &= ~(1 << PD3);
   }
 
-  if (currentMicros - previousMicros5 >= 1000) {
+  if (currentMicros - previousMicros5 >= 500) {
     previousMicros5 = currentMicros;
     pinState5 = !pinState5;
-    digitalWrite(5, pinState5);
+    if (pinState5) PORTD |= (1 << PD5);
+    else PORTD &= ~(1 << PD5);
   }
 
-  if (currentMicros - previousMicros6 >= 500) {
+  if (currentMicros - previousMicros6 >= 250) {
     previousMicros6 = currentMicros;
     pinState6 = !pinState6;
-    digitalWrite(6, pinState6);
+    if (pinState6) PORTD |= (1 << PD6);
+    else PORTD &= ~(1 << PD6);
   }
 
-  if (currentMicros - previousMicros9 >= 100) {
+  if (currentMicros - previousMicros9 >= 50) {
     previousMicros9 = currentMicros;
     pinState9 = !pinState9;
-    digitalWrite(9, pinState9);
+    if (pinState9) PORTB |= (1 << PB1);
+    else PORTB &= ~(1 << PB1);
   }
 
-  if (currentMicros - previousMicros10 >= 50) {
+  if (currentMicros - previousMicros10 >= 25) {
     previousMicros10 = currentMicros;
     pinState10 = !pinState10;
-    digitalWrite(10, pinState10);
+    if (pinState10) PORTB |= (1 << PB2);
+    else PORTB &= ~(1 << PB2);
   }
 }
